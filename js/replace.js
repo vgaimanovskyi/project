9ec25f02f963da96d3d0b92d1d4f3807a0f13714
vgaimanovskyi <vgaymanovskiy@gmail.com>
2020-07-14 21:12:10 +0300

    add mobile.html

--- js/replace.js
+++ js/replace.js
@@ -3,8 +3,11 @@ function replace() {
     if (!!window.MSInputMethodContext && !!document.documentMode) {
         window.location.replace("pages/ie.html");
     }
-    if (navigator.userAgent.toLowerCase().indexOf('msie') != -1) {
+    if (navigator.userAgent.toLowerCase().indexOf("msie") != -1) {
         window.location.replace("pages/ie.html");
+    }
+    if (document.body.clientWidth < 479) {
+        window.location.replace("pages/mobile.html");
     }
 };
 replace();
